ceecd67b1702b93332ffbc21d4d9da4f8a40ba16
Yanis Verbeke <yanis88357@gmail.com>
2020-02-18 15:55:35 +0100

    Add Player

---
+++ gamingProj/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float m_TranslationSpeed;
+
+    Rigidbody m_RigidBody;
+
+    void Awake()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+        Vector3 vectForward = transform.forward * m_TranslationSpeed * Time.fixedDeltaTime * vInput;
+        Vector3 vectSide = transform.right * m_TranslationSpeed * Time.fixedDeltaTime * hInput;
+        m_RigidBody.MovePosition(transform.position + vectForward + vectSide);
+    }
+}
