a916cc05cddc88cff31ca606d5dcf9e60af6ad74
Melanie V <melanie.vaubien@hotmail.com>
2020-02-19 17:27:42 +0100

    Menu Play

---
+++ gamingProj/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class LoadGame : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Exit);
+    }
+    public void Exit()
+    {
+        SceneManager.LoadScene("TestPhysics");
+    }
+}
