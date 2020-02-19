1a6c9a8824bc5dd2c9935937b7f3c447b9854daf
Melanie V <melanie.vaubien@hotmail.com>
2020-02-19 16:51:26 +0100

    Boutton Restart

--- gamingProj/Assets/Scripts/Player.cs
+++ gamingProj/Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +9,9 @@ public class Player : MonoBehaviour
 
     private Rigidbody m_RigidBody;
 
+    [SerializeField]
     private int HealthPoint = 3;
+    public GameObject ResetButton;
 
     void Awake()
     {
@@ -19,24 +21,32 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(HealthPoint <= 0)
+        {
+            Destroy(this.gameObject);
+            Instantiate(ResetButton);
+        }
     }
     void FixedUpdate()
     {
         float vInput = Input.GetAxis("Vertical");
         float hInput = Input.GetAxis("Horizontal");
-        Vector3 dir = Vector3.ClampMagnitude(new Vector3(hInput, 0, vInput),1);
-        
+        Vector3 dir = Vector3.ClampMagnitude(new Vector3(hInput, 0, vInput), 1);
+
         //Vector3 vectForward = transform.forward * m_TranslationSpeed * Time.fixedDeltaTime * vInput;
         //Vector3 vectSide = transform.right * m_TranslationSpeed * Time.fixedDeltaTime * hInput;
-        m_RigidBody.MovePosition(transform.position + dir * m_TranslationSpeed * Time.fixedDeltaTime);
-        m_RigidBody.velocity = Vector3.zero;
+        if (HealthPoint > 0)
+        {
+            m_RigidBody.MovePosition(transform.position + dir * m_TranslationSpeed * Time.fixedDeltaTime);
+            m_RigidBody.velocity = Vector3.zero;
+        }
+
     }
 
     void HealthDown()
