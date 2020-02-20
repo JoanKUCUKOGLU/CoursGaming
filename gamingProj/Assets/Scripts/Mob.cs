50894388702d1a4ba83c4f628b48052973563940
Joan Kucukoglu <joankucukoglu@gmail.com>
2020-02-20 16:19:44 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- gamingProj/Assets/Scripts/Mob.cs
+++ gamingProj/Assets/Scripts/Mob.cs
@@ -16,6 +16,8 @@ public class Mob : MonoBehaviour
     [SerializeField]
     private NavMeshAgent agent;
 
+    bool isTranslating = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,48 +27,32 @@ public class Mob : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, mobSpeed);
-
-        //Debug.Log(player.transform.position);
-
         if(Time.frameCount % 20 == 0)
         {
+            agent.speed = !isTranslating ? 10 : 1;
             agent.SetDestination(player.transform.position);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Equals("Player"))
+        if (collision.gameObject.name.Equals("Player") && !isTranslating)
         {
             if (player != null)
             {
                 player.SendMessage("HealthDown");
-                //GetComponent<Rigidbody>().AddForce(-transform.forward * 1.5F);
+                StartCoroutine(TranslationCoroutine());
             }
         }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name.Equals("Player") && !isTranslating)
-        {
-            player.SendMessage("HealthDown");
-            StartCoroutine(TranslationCoroutine());
-        }
-    }
 
-    bool isTranslating = false;
 
     IEnumerator TranslationCoroutine() 
     {
         float elapsedTime = 0;
-        //isTranslating = true;
+        isTranslating = true;
 
-        //Vector3 direction = (transform.position - player.transform.position).normalized;
-
-        //GetComponent<Rigidbody>().velocity = direction * 100;
-
-        while (elapsedTime < 0.5)
+        while (elapsedTime < 1)
         {
             elapsedTime += Time.deltaTime;
             yield return null;
