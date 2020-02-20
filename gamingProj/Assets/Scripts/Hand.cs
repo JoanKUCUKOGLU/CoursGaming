5735b0e35c6bf95fc69d829801639d77a643ffc8
Unknown <anthony.guiock@edu.itescia.fr>
2020-02-20 10:56:29 +0100

    Add Knife + clean weapons

--- gamingProj/Assets/Scripts/Hand.cs
+++ gamingProj/Assets/Scripts/Hand.cs
@@ -8,27 +8,31 @@ public class Hand : MonoBehaviour
     float internalClock = 0;
     float shootingAngle;
     GameObject gun;
+    bool isFixed = false;
+    Vector3 shootPos;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        gun = GameObject.Find("Knife");
     }
 
     // Update is called once per frame
     void Update()
     {
-        shootingAngle = Mathf.Atan2(Input.GetAxis("R3X"), Input.GetAxis("R3Y")) * Mathf.Rad2Deg;
-        transform.position = new Vector3(Input.GetAxis("R3X") * 0.7f + transform.parent.position.x, transform.position.y, Input.GetAxis("R3Y") * 0.7f + transform.parent.position.z);
-        transform.rotation = Quaternion.Euler(0, shootingAngle, 0);
-
-        if ((Input.GetAxis("R3X") != 0 || Input.GetAxis("R3Y") != 0) && internalClock >= cooldown)
+        if (!isFixed)
         {
-            //if gun != null
-            gun.SendMessage("Shoot");   //Appelle la fonction "Shoot" de tout les composants du gun actuel
-            internalClock = 0;
+            shootingAngle = Mathf.Atan2(Input.GetAxis("R3X"), Input.GetAxis("R3Y")) * Mathf.Rad2Deg;
+            shootPos = new Vector3(Input.GetAxis("R3X") * 0.8f + transform.parent.position.x, transform.position.y, Input.GetAxis("R3Y") * 0.8f + transform.parent.position.z);
+            transform.rotation = Quaternion.Euler(0, shootingAngle, 0);
+
+            if ((Input.GetAxis("R3X") != 0 || Input.GetAxis("R3Y") != 0) && (internalClock >= cooldown || cooldown == 0))
+            {
+                gun.SendMessage("Shoot",shootPos);   //Appelle la fonction "Shoot" de tout les composants du gun actuel
+                internalClock = 0;
+            }
+            internalClock += Time.deltaTime;
         }
-        internalClock += Time.deltaTime;
     }
 
     /// <summary>
@@ -37,9 +41,7 @@ public class Hand : MonoBehaviour
     /// <param name="gun">la nouvelle arme active</param>
     void SetGun(GameObject gun)
     {
-        Debug.Log("Gun changed for " + gun.name);
         this.gun = gun;
-        Debug.Log("Gun is now " + this.gun.name);
     }
 
     /// <summary>
@@ -48,9 +50,16 @@ public class Hand : MonoBehaviour
     /// <param name="newCooldown">le nouveau cooldown à appliquer</param>
     void SetCooldown(float newCooldown)
     {
-        Debug.Log("Cooldown changed for " + newCooldown);
+        internalClock = 0;
         cooldown = newCooldown;
-        Debug.Log("Cooldown is now " + cooldown);
     }
-   
+
+    /// <summary>
+    /// Bloque la rotation de la main (utilisée pour le couteau)
+    /// </summary>
+    /// <param name="value">true pour bloquer la rotation de la main</param>
+    void SetIsFixed(bool value)
+    {
+        isFixed = value;
+    }
 }
---
+++ gamingProj/Assets/Scripts/Knife.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : Weapons
+{
+    [SerializeField]
+    float distance = 1;
+    [SerializeField]
+    float speed = 1;
+
+    bool isFiring = false;
+    bool isRetiring = false;
+    int i = 0;
+
+    protected override void Shoot(Vector3 shootPos)
+    {
+        if (!isFiring && !isRetiring)
+        {
+            isFiring = true;
+        }
+    }
+
+    private void Update()
+    {
+        float travelDist = i * speed * Time.deltaTime;
+        if (isFiring)
+        {
+            transform.parent.SendMessage("SetIsFixed", true);
+            collider.enabled = true;
+            meshRenderer.enabled = true;
+            transform.position += transform.parent.forward * Mathf.Lerp(0, distance, travelDist);
+            if (travelDist >= 0.1)
+            {
+                isFiring = false;
+                isRetiring = true;
+            }
+            i++;
+        }
+        else if (isRetiring)
+        {
+            i--;
+            transform.position -= transform.parent.forward * Mathf.Lerp(0, distance, travelDist);
+            if (travelDist <= 0)
+            {
+                isFiring = false;
+                isRetiring = false;
+                transform.parent.SendMessage("SetIsFixed", false);
+                collider.enabled = false;
+                meshRenderer.enabled = false;
+                transform.position = transform.parent.position;
+            }
+        }
+    }
+}
